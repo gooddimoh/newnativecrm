3915dcc382dbb66ca5375c522cd6726120bd9adb
Dmitriy Balovnev <balovnev.dmitriy@gmail.com>
2023-01-14 14:54:11 +0200

    first commit

---
+++ queries.php
@@ -0,0 +1,23 @@
+<?php
+
+$connection = mysqli_connect('', '', '', '', '', '');
+
+$enable = true;
+$connection->begin_transaction($enable);
+$connection->autocommit($enable);
+try
+$connection->query('select * from users');
+$connection->query('select * from users');
+$connection->query('select * from users');
+$connection->query('select * from users');
+$connection->query('select * from users');
+$connection->query('select * from users');
+$connection->query('select * from users');
+$connection->query('select * from users');
+$connection->query('select * from users');
+$connection->query('select * from users');
+$connection->query('select * from users');
+$connection->query('select * from users');
+$connection->query('select * from users');
+$connection->query('select * from users');
+$connection->prepare();
---
+++ run.sh
@@ -0,0 +1,7 @@
+echo "# newnativecrm" >> README.md
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/gooddimoh/newnativecrm.git
+git push -u origin main
